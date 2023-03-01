a6bc389abc5a91c901937d10d09ec4eeb5ae02e5
ilhamrizki <irp100202@gmail.com>
2023-03-01 12:20:32 +0700

    authentication implemented

--- migrations/1677507185280_create-table-users.js
+++ migrations/1677507185280_create-table-users.js
@@ -8,19 +8,23 @@ exports.up = (pgm) => {
       type: 'VARCHAR(50)',
       primaryKey: true,
     },
+
     username: {
       type: 'VARCHAR(50)',
       unique: true,
       notNull: true,
     },
+
     password: {
       type: 'TEXT',
       notNull: true,
     },
+
     fullname: {
       type: 'TEXT',
       notNull: true,
     },
+    
   });
 };
  
